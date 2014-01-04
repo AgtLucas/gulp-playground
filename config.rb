9a0f6ebed81208924df270cde6201e1306b1a954
Lucas <lucas@agtlucas.com>
2014-01-04 16:22:49 -0200

    Update config.rb

---
+++ config.rb
@@ -0,0 +1,10 @@
+prefered_syntax = :scss
+http_path = '/'
+css_dir = 'css'
+sass_dir = 'scss'
+images_dir = 'img'
+javascript_dir = 'js'
+relative_assets = true
+line_comments = true
+output_style = :compressed
+require "susy"
