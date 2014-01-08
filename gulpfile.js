538403c4d5d51c6e3e7912a7d7279ad38f9a321b
Lucas <lucas@agtlucas.com>
2014-01-08 09:01:22 -0200

    Update Gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,13 @@ gulp.task('scripts', function() {
       .pipe(gulp.dest('./dist'));
 });
 
+// Server
+gulp.task('livereload', function() {
+  server.listen(35729, function(err) {
+    if (err) return console.log(err);
+  });
+});
+
 // Default Task
 gulp.task('default', function() {
   gulp.run('lint', 'compass', 'scripts');
