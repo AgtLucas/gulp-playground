c1bba3f176bc6733ba947c9ab07b179aae9e5ecd
Lucas Churchill <lucas@agtlucas.com>
2014-02-22 20:56:07 -0300

    Update gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Load plugins
+ */
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var jshint = require('gulp-jshint');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var cache = require('gulp-cache');
+var livereload = require('gulp-livereload');
+var lr = require('tiny-lr');
+var server = lr();
