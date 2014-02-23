ccded8c55d010465e762703571f2079e74631fd0
Lucas Churchill <lucas@agtlucas.com>
2014-02-23 11:12:35 -0300

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ var server = lr();
  * Styles task
  */
 gulp.task('styles', function () {
-  return gulp.src('assets/scss/style.scss')
+  return gulp.src('assets/scss/**/*.scss')
     .pipe(sass({ style: 'compressed' }))
     .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('build/css'))
